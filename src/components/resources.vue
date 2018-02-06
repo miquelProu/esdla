0a266e00e7622ca3174952564a2ee5ab2ddcc75e
Miquel-BR <miquel.domingo@bcnregional.com>
2018-02-06 14:43:03 +0100

    Per poder seguir picant

--- src/components/resources.vue
+++ src/components/resources.vue
@@ -1,8 +1,16 @@
 <template>
-    <div class="resource is-clearfix">
-        <img src="../assets/markers/62a2ba76-9872-481b-b8fc-ec35447ca640.png"/>
-        <div>{{value}}</div>
-    </div>
+    <nav class="level" style="position:absolute;">
+        <!-- Left side -->
+        <div class="level-left">
+            <div class="level-item has-text-left" xstyle="text-align: left;">
+                <!--<div class="resource is-clearfix">-->
+                <img class="" style="width: 30%;height: 30%;" src="../assets/markers/62a2ba76-9872-481b-b8fc-ec35447ca640.png"/>
+            </div>
+           <div class="level-item">
+               <div>{{value}}</div>
+           </div>
+        </div>
+    </nav>
 </template>
 
 <script>
@@ -29,18 +37,20 @@
         left:0;
         margin-top: -22px;
 
-        img {
-            min-width: 10px;
-            min-height: 10px;
-            width: 30%;
-            height: 30%;
-            display:contents;
-            float: left;
-        }
+
 
         div {
             float: left;
             margin-left: 5px;
         }
+    }
+
+    img {
+        min-width: 10px;
+        min-height: 10px;
+        width: 30%;
+        height: 30%;
+        /*display:contents;*/
+        /*float: left;*/
     }
 </style>
